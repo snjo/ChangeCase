d4a86f0e243b5b6961ce5faf581be86f24e91316
Andreas Aakvik Gogstad <agogstad@gmail.com>
2021-11-25 16:50:01 +0100

    Added hotkey support
    Added hotkeys for Upper/Lower casing the clipboard.
    Set auto mode to off on start.

--- ChangeCaseGUI/Form1.Designer.cs
+++ ChangeCaseGUI/Form1.Designer.cs
@@ -34,6 +34,9 @@
             this.radioLower = new System.Windows.Forms.RadioButton();
             this.radioUpper = new System.Windows.Forms.RadioButton();
             this.radioOff = new System.Windows.Forms.RadioButton();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // timer1
@@ -44,12 +47,10 @@
             // radioLower
             // 
             this.radioLower.AutoSize = true;
-            this.radioLower.Checked = true;
             this.radioLower.Location = new System.Drawing.Point(12, 35);
             this.radioLower.Name = "radioLower";
             this.radioLower.Size = new System.Drawing.Size(76, 17);
             this.radioLower.TabIndex = 2;
-            this.radioLower.TabStop = true;
             this.radioLower.Text = "lower case";
             this.radioLower.UseVisualStyleBackColor = true;
             // 
@@ -66,6 +67,7 @@
             // radioOff
             // 
             this.radioOff.AutoSize = true;
+            this.radioOff.Checked = true;
             this.radioOff.Location = new System.Drawing.Point(12, 12);
             this.radioOff.Name = "radioOff";
             this.radioOff.Size = new System.Drawing.Size(37, 17);
@@ -74,17 +76,49 @@
             this.radioOff.Text = "off";
             this.radioOff.UseVisualStyleBackColor = true;
             // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(137, 61);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(57, 13);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "Alt+Shit+U";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(137, 39);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(55, 13);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "Alt+Shit+L";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(137, 16);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(65, 13);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "On demand:";
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(206, 90);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
             this.Controls.Add(this.radioOff);
             this.Controls.Add(this.radioUpper);
             this.Controls.Add(this.radioLower);
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.Name = "Form1";
             this.Text = "Case converter";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
+            this.Load += new System.EventHandler(this.Form1_Load);
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -96,6 +130,9 @@
         private System.Windows.Forms.RadioButton radioLower;
         private System.Windows.Forms.RadioButton radioUpper;
         private System.Windows.Forms.RadioButton radioOff;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
     }
 }
 
--- ChangeCaseGUI/Form1.cs
+++ ChangeCaseGUI/Form1.cs
@@ -7,19 +7,41 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using Hotkeys;
 
 namespace ChangeCaseGUI
 {
     public partial class Form1 : Form
     {
         string clipBoardText;
-         
+        private Hotkeys.GlobalHotkey ghkUpper;
+        private Hotkeys.GlobalHotkey ghkLower;
+
         public Form1()
         {
             InitializeComponent();
 
             timer1.Start();
- 
+            ghkUpper = new Hotkeys.GlobalHotkey(Constants.ALT + Constants.SHIFT, Keys.U, this);
+            ghkLower = new Hotkeys.GlobalHotkey(Constants.ALT + Constants.SHIFT, Keys.L, this);
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            //trying to register hotkey
+            if (!ghkUpper.Register())
+                writeMessage("HkUpper not registered");
+            if (!ghkLower.Register())
+                writeMessage("HkLower not registered");
+        }
+
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            if (!ghkUpper.Unregister())
+            {
+                MessageBox.Show("Hotkey failed to unregister");
+                //writeMessage("Hotkey failed to unregister");
+            }
         }
 
         private void changeCase()
@@ -54,5 +76,46 @@ namespace ChangeCaseGUI
         {
             changeCase();
         }
+
+        private void HandleHotkey(int id)
+        {
+            //hotkey pressed
+            //MessageBox.Show("Hotkey pressed");
+
+            //writeMessage("Hotkey pressed");
+
+            if (id == ghkLower.id)
+            {
+                clipBoardText = Clipboard.GetText(TextDataFormat.Text);
+                clipBoardText = clipBoardText.ToLower();
+                Clipboard.SetText(clipBoardText);
+            }
+
+            if (id == ghkUpper.id)
+            {
+                clipBoardText = Clipboard.GetText(TextDataFormat.Text);
+                clipBoardText = clipBoardText.ToUpper();
+                Clipboard.SetText(clipBoardText);
+            }
+        }
+
+        private void writeMessage(string text)
+        {
+            label1.Text = text;
+        }
+
+        protected override void WndProc(ref Message m)
+        {
+            base.WndProc(ref m);            
+            if (m.Msg == Hotkeys.Constants.WM_HOTKEY_MSG_ID)
+            {
+                Keys key = (Keys)(((int)m.LParam >> 16) & 0xFFFF);                  // The key of the hotkey that was pressed.
+                KeyModifier modifier = (KeyModifier)((int)m.LParam & 0xFFFF);       // The modifier of the hotkey that was pressed.
+                int id = m.WParam.ToInt32();                                        // The id of the hotkey that was pressed.
+                //MessageBox.Show("Hotkey " + id + " has been pressed!");
+                HandleHotkey(id);
+            }            
+        }
+
     }
 }
